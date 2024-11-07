639d01977f7a10a756cc5e51daa20f04f3898cb7
Thavillalo <00001129508079sp@al.educacao.sp.gov.br>
2024-11-07 12:24:18 +0000

    Pending changes exported from your codespace

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+        const detalhesBtns = document.querySelectorAll(".detalhes-btn");
+
+            detalhesBtns.forEach(btn => {
+                    btn.addEventListener("click", () => {
+                                alert("Detalhes do livro ainda não disponíveis.");
+                                        });
+                                            });
+                                            });
